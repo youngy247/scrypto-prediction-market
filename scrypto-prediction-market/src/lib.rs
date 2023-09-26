a737b333e4ca1261c3aa04ac5c104894a5897136
Adam Young <adamyoungy678@gmail.com>
2023-09-26 15:33:43 +0100

    Feat: Implement instantiate_prediction_market Function

--- scrypto-prediction-market/src/lib.rs
+++ scrypto-prediction-market/src/lib.rs
@@ -7,4 +7,33 @@ mod prediction_market {
         outcomes: Vec<String>,
         total_staked: Decimal,
     }
+
+    impl PredictionMarket {
+        pub fn instantiate_prediction_market(outcomes: Vec<String>) -> Global<PredictionMarket> {
+            let mut outcome_tokens = Vec::new();
+
+            for outcome in &outcomes {
+                let token_name = format!("PredictionToken_{}", outcome);
+                let token = ResourceBuilder::new_fungible(OwnerRole::None)
+                    .metadata(metadata!(
+                        init {
+                            "name" => token_name.clone(), locked;
+                            "symbol" => outcome.clone(), locked;
+                            "description" => format!("Token for predicting {}", outcome), locked;
+                        }
+                    ))
+                    .mint_initial_supply(0); // Initialize with zero tokens
+                outcome_tokens.push(Vault::with_bucket(token.into()));
+            }
+
+            Self {
+                outcome_tokens,
+                outcomes,
+                total_staked: Decimal::from(0),
+            }
+            .instantiate()
+            .prepare_to_globalize(OwnerRole::None)
+            .globalize()
+        }
+    }
 }
