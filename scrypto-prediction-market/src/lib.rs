42700bc6ec2f8ae4698677fe8c1063b788a4a379
Adam Young <adamyoungy678@gmail.com>
2023-09-27 08:50:56 +0100

    Refactor: Restrict outcome vaults to only accept XRD tokens

--- scrypto-prediction-market/src/lib.rs
+++ scrypto-prediction-market/src/lib.rs
@@ -12,21 +12,11 @@ mod prediction_market {
     impl PredictionMarket {
         pub fn instantiate_prediction_market(outcomes: Vec<String>) -> Global<PredictionMarket> {
             let mut outcome_tokens = Vec::new();
-
-            for outcome in &outcomes {
-                let token_name = format!("PredictionToken_{}", outcome);
-                let token = ResourceBuilder::new_fungible(OwnerRole::None)
-                    .metadata(metadata!(
-                        init {
-                            "name" => token_name.clone(), locked;
-                            "symbol" => outcome.clone(), locked;
-                            "description" => format!("Token for predicting {}", outcome), locked;
-                        }
-                    ))
-                    .mint_initial_supply(0); // Initialize with zero tokens
-                outcome_tokens.push(Vault::with_bucket(token.into()));
+        
+            for _ in &outcomes {
+                outcome_tokens.push(Vault::new(XRD)); // Create a new XRD vault for each outcome
             }
-
+        
             Self {
                 outcome_tokens,
                 outcomes,
@@ -37,6 +27,7 @@ mod prediction_market {
             .prepare_to_globalize(OwnerRole::None)
             .globalize()
         }
+        
 
         pub fn list_outcomes(&self) -> Vec<String> {
             self.outcomes.clone()
