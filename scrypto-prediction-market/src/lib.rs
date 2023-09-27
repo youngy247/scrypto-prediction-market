d06289fe39cad8bfb3a6b1523365d092b249de57
Adam Young <adamyoungy678@gmail.com>
2023-09-27 19:47:39 +0100

    chore: code cleanup

--- scrypto-prediction-market/src/lib.rs
+++ scrypto-prediction-market/src/lib.rs
@@ -19,7 +19,7 @@ mod prediction_market {
             for _ in &outcomes {
                 outcome_tokens.push(Vault::new(XRD)); // Create a new XRD vault for each outcome
             }
-        
+            
             Self {
                 outcome_tokens,
                 outcomes,
@@ -32,10 +32,10 @@ mod prediction_market {
             .globalize()
         }
         
-
         pub fn list_outcomes(&self) -> Vec<String> {
             self.outcomes.clone()
         }
+
         pub fn get_total_staked(&self) -> Decimal {
             self.total_staked.clone()
         }
@@ -48,33 +48,23 @@ mod prediction_market {
 
         pub fn resolve_market(&mut self, winning_outcome: u32) -> Vec<(String, Decimal)> {
             if (winning_outcome as usize) < self.outcome_tokens.len() {
-                // Calculate the reward for each participant
                 let mut rewards = Vec::new();
                 for (index, outcome_token) in self.outcome_tokens.iter_mut().enumerate() {
                     let bet_amount: Bucket = outcome_token.take_all();
-
                     let outcome = &self.outcomes[index];
                     let reward = if index == winning_outcome as usize {
-                        // If it's the winning outcome, distribute the entire pot to participants
                         self.total_staked.clone()
                     } else {
-                        // Otherwise, return the bet amount as is
                         Decimal::from(bet_amount.amount())
                     };
-
                     rewards.push((outcome.clone(), reward));
                 }
-
-                // Reset the market after resolution
                 for t in &mut self.outcome_tokens {
-                    drop(t.take_all()); // Drop the Bucket value explicitly
+                    drop(t.take_all());
                 }
                 self.total_staked = Decimal::from(0);
-
                 return rewards;
             }
-
-            // Invalid winning outcome
             Vec::new()
         }
 
@@ -114,12 +104,12 @@ mod prediction_market {
 
         let taken_bucket = self.xrd_vault.take(bet_amount); // Here, 'take' should be returning a bucket of the specified amount.
         
-            if let Some(index) = self.outcomes.iter().position(|o| o == &outcome) {
-                let outcome_token = &mut self.outcome_tokens[index];
-                outcome_token.put(taken_bucket);
-                self.total_staked += bet_amount;
-                return true;
-            } else {
+        if let Some(index) = self.outcomes.iter().position(|o| o == &outcome) {
+            let outcome_token = &mut self.outcome_tokens[index];
+            outcome_token.put(taken_bucket);
+            self.total_staked += bet_amount;
+            return true;
+        } else {
             self.xrd_vault.put(taken_bucket);  // Return the taken bucket if the outcome doesn't exist
             return false;
         }
@@ -136,4 +126,4 @@ mod prediction_market {
             }
         }
     }
-}
+    }
