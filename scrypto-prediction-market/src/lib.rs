5d35e94b8c0a7ff0619850b29515f6cf5fd0514f
Adam Young <adamyoungy678@gmail.com>
2023-09-28 11:24:20 +0100

    Feat: Implement method for users to claim rewards

--- scrypto-prediction-market/src/lib.rs
+++ scrypto-prediction-market/src/lib.rs
@@ -61,61 +61,10 @@ mod prediction_market {
             self.xrd_vault.put(deposit);
         }
 
-        pub fn deposit(&mut self, account_id: String, payment: Bucket) -> Result<(), String> {
-            // Try to take the deposit amount from the user's default payment bucket
-            let deposit_amount = payment.amount();
-            
-            // Check if the deposit was successful (i.e., the user had enough balance)
-            if deposit_amount > Decimal::from(0) {
-            // Add the payment bucket to the component's xrd_vault
-            self.xrd_vault.put(payment);
 
-            // Add or update the user's balance in the users Vec.
-            self.add_or_update_user_balance(account_id.clone(), deposit_amount);
-
-            Ok(())
-        } else {
-            Err(format!("Invalid or insufficient XRD payment for user: {}", account_id))
-        }
-        }
-        
-        
-    pub fn get_user_balance(&self, user_hash: String) -> Option<Decimal> {
-        self.users.iter().find(|(u, _)| u == &user_hash).map(|(_, balance)| balance.clone())
-    }
-
-            
-    pub fn place_bet(&mut self, user_hash: String, outcome: String, bet_amount: Decimal) -> bool {
-        // Check if the user has sufficient balance
-        if let Some((_, balance)) = self.users.iter().find(|(u, _)| u == &user_hash) {
-            if *balance < bet_amount {
-                return false; // Insufficient funds
-            }
-        } else {
-            return false; // User not found
-        }
-
-        let taken_bucket = self.xrd_vault.take(bet_amount); // Here, 'take' should be returning a bucket of the specified amount.
-        
-        if let Some(index) = self.outcomes.iter().position(|o| o == &outcome) {
-            let outcome_token = &mut self.outcome_tokens[index];
-            outcome_token.put(taken_bucket);
-            self.total_staked += bet_amount;
-            return true;
-        } else {
-            self.xrd_vault.put(taken_bucket);  // Return the taken bucket if the outcome doesn't exist
-            return false;
-        }
-    }
-
-
-            /// Adds or updates a user's balance. If the user already exists, their balance will be increased by `amount`.
-            /// Otherwise, a new user entry is created with the specified `amount`.
-        pub fn add_or_update_user_balance(&mut self, user_hash: String, amount: Decimal) {
-            if let Some((_, balance)) = self.users.iter_mut().find(|(u, _)| u == &user_hash) {
-                *balance += amount;
-            } else {
-                self.users.push((user_hash, amount));
+        // Add a new method for users to claim their rewards from their vaults.
+        pub fn claim_reward(&mut self, user_hash: String) -> Option<Bucket> {
+            self.user_vaults.get_mut(&user_hash).map(|vault| vault.take_all())
             }
         }
     }
