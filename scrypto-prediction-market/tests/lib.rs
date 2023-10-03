f0445c2aefa508ed5fbd9a76c2dfdcd8d415612b
Adam Young <adamyoungy678@gmail.com>
2023-10-03 10:52:50 +0100

    Refactor: updated unit tests to account for new arguments in instantiation

--- scrypto-prediction-market/tests/lib.rs
+++ scrypto-prediction-market/tests/lib.rs
@@ -14,30 +14,32 @@ fn test_instantiate_prediction_market() -> Result<(), RuntimeError> {
     let package_address = test_runner.compile_and_publish(this_package!());
 
     // Define outcomes and odds
+    let title= "title".to_string();
     let outcomes_str = "outcome1,outcome2".to_string();
     let odds_str = "2,3".to_string();
-
+    let min_bet = dec!("5");
+    let max_bet = dec!("100");
     // Instantiate the PredictionMarket via a Manifest
     let manifest1 = ManifestBuilder::new()
         .call_function(
             package_address,
             "PredictionMarket",
             "instantiate_prediction_market",
-            manifest_args!(outcomes_str, odds_str),
+            manifest_args!(title, outcomes_str, odds_str, min_bet, max_bet),
         )
         .call_method(
-          account_component,
-          "deposit_batch",
-          manifest_args!(ManifestExpression::EntireWorktop),
-      )
+                account_component,
+            "deposit_batch",
+            manifest_args!(ManifestExpression::EntireWorktop),
+    )
         .build();
     
         let receipt1 = test_runner.execute_manifest_ignoring_fee(
-          manifest1,
-          vec![NonFungibleGlobalId::from_public_key(&public_key)],
-      );
-      println!("{:?}\n", receipt1);
-      receipt1.expect_commit_success();
+            manifest1,
+            vec![NonFungibleGlobalId::from_public_key(&public_key)],
+        );
+    println!("{:?}\n", receipt1);
+    receipt1.expect_commit_success();
     
     // ... [If needed to use the badge later]
     // let use_badge_manifest = ManifestBuilder::new()
@@ -59,22 +61,34 @@ fn test_list_outcomes() -> Result<(), RuntimeError> {
     let (public_key, _private_key, account_component) = test_runner.new_allocated_account();
     let package_address = test_runner.compile_and_publish(this_package!());
 
-    // Instantiate the PredictionMarket
+    // Define outcomes and odds
+    let title = "title".to_string();
     let outcomes_str = "outcome1,outcome2".to_string();
     let odds_str = "2,3".to_string();
+    let min_bet = dec!("5");
+    let max_bet = dec!("100");
+
+    // Instantiate the PredictionMarket
     let manifest = ManifestBuilder::new()
         .call_function(
             package_address,
             "PredictionMarket",
             "instantiate_prediction_market",
-            manifest_args!(outcomes_str.clone(), odds_str.clone()),
+            manifest_args!(
+                title.clone(),
+                outcomes_str.clone(),
+                odds_str.clone(),
+                min_bet.clone(),
+                max_bet.clone()
+            ),
         )
         .call_method(
-          account_component,
-          "deposit_batch",
-          manifest_args!(ManifestExpression::EntireWorktop),
-      )
+            account_component,
+            "deposit_batch",
+            manifest_args!(ManifestExpression::EntireWorktop),
+        )
         .build();
+
     let receipt = test_runner.execute_manifest_ignoring_fee(manifest, vec![NonFungibleGlobalId::from_public_key(&public_key)]);
     let market_address = receipt.expect_commit(true).new_component_addresses()[0];
 
